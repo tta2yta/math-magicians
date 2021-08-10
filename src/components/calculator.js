4a7f46b1b1eabe5d1c13b06fcb267ac097c7f139
tta2yta <tta2yta@gmail.com>
2021-08-11 00:51:15 +0300

    Update calculator class

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 
 export default class Calculator extends React.Component {
@@ -10,12 +11,38 @@ export default class Calculator extends React.Component {
     return (
       <div className="calculator">
         <div className="calculator-body">
-            <div className="calc-row"></div>
-            <div className="calc-row"></div>
-            <div className="calc-row"></div>
-            <div className="calc-row"></div>
-            <div className="calc-row"></div>
-            </div>
+          <div className="calc-row">AC</div>
+          <div className="calc-row">+/-</div>
+          <div className="calc-row">%</div>
+          <div className="calc-row">*</div>
+        </div>
+
+        <div className="calculator-body">
+          <div className="calc-row">AC</div>
+          <div className="calc-row">+/-</div>
+          <div className="calc-row">%</div>
+          <div className="calc-row">/</div>
+        </div>
+
+        <div className="calculator-body">
+          <div className="calc-row">7</div>
+          <div className="calc-row">8</div>
+          <div className="calc-row">9</div>
+          <div className="calc-row">*</div>
+        </div>
+
+        <div className="calculator-body">
+          <div className="calc-row">1</div>
+          <div className="calc-row">2</div>
+          <div className="calc-row">3</div>
+          <div className="calc-row">+</div>
+        </div>
+
+        <div className="calculator-body-last">
+          <div className="calc-row cols-span-2">0</div>
+          <div className="calc-row">.</div>
+          <div className="calc-row">=</div>
+        </div>
       </div>
     );
   }
