fa5a56412870f9d2b7d316eb3594384b88419cc9
tta2yta <tta2yta@gmail.com>
2021-08-11 01:52:03 +0300

    Update css for calculator clas

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -19,34 +19,34 @@ export default class Calculator extends React.Component {
           <div className="calc-row">AC</div>
           <div className="calc-row">+/-</div>
           <div className="calc-row">%</div>
-          <div className="calc-row">*</div>
+          <div className="calc-row arthemetic">*</div>
         </div>
 
         <div className="calculator-body">
           <div className="calc-row">AC</div>
           <div className="calc-row">+/-</div>
           <div className="calc-row">%</div>
-          <div className="calc-row">/</div>
+          <div className="calc-row arthemetic">/</div>
         </div>
 
         <div className="calculator-body">
           <div className="calc-row">7</div>
           <div className="calc-row">8</div>
           <div className="calc-row">9</div>
-          <div className="calc-row">*</div>
+          <div className="calc-row arthemetic">*</div>
         </div>
 
         <div className="calculator-body">
           <div className="calc-row">1</div>
           <div className="calc-row">2</div>
           <div className="calc-row">3</div>
-          <div className="calc-row">+</div>
+          <div className="calc-row  arthemetic">+</div>
         </div>
 
         <div className="calculator-body-last">
           <div className="calc-row cols-span-2">0</div>
           <div className="calc-row">.</div>
-          <div className="calc-row">=</div>
+          <div className="calc-row arthemetic">=</div>
         </div>
       </div>
     );
