c8e17d458fca6911b1f34d59fdd5d2b72b72153d
tta2yta <tta2yta@gmail.com>
2021-08-13 13:04:57 +0300

    Update handleClick and input value property

--- src/components/calculatorHooks.js
+++ src/components/calculatorHooks.js
@@ -3,7 +3,7 @@ import {useState, useEffect, useRef } from 'react';
 import calculate from '../logic/calculate';
 
 export function Calculator() {
-  const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(null);
   const [next, setNext] = useState(0);
   const [operation, setOperation] = useState(null);
   const prevCountRef = useRef();
@@ -11,21 +11,30 @@ export function Calculator() {
   const handleClick = (e) => {
     if (isNumber(e.target.outerText)) {
         console.log(e.target.outerText)
-        const prevNext = prevCountRef.current;
-        console.log(prevNext)
         setNext(e.target.value)
         const obj = {total, next, operation}
         console.log(obj)
         const res = calculate(obj, e.target.outerText);
         console.log(res)
         setNext(res.next)
+        
       }
 
       if (e.target.outerText === '+' || e.target.outerText === '-' || e.target.outerText === 'x' || e.target.outerText === '÷') {
         // this.setState({ operation: e.target.outerText });
+        let obj={}
+        if (e.target.outerText === 'x' || e.target.outerText === '÷'){
+            setTotal(total=== null ? 1 : total);
+             obj = {total:total=== null ? 1 : total, next: next===null? 1: next, operation:e.target.outerText}
+        }
+        if (e.target.outerText === '+' || e.target.outerText === '-'){
+            setTotal(total=== null ? 0 : total);
+             obj = {total:total=== null ? 0 : total, next: next===null? 0 : next, operation:e.target.outerText}
+        }
+        console.log(total)
         setOperation(e.target.outerText)
         console.log(e.target.outerText)
-        const obj = {total, next, operation:e.target.outerText}
+        
         console.log(obj)
         const res = calculate(obj, e.target.outerText);
         // this.setState({ next: res.next });
@@ -40,9 +49,20 @@ export function Calculator() {
         console.log(obj)
         const res = calculate(obj, e.target.outerText);
         console.log(res)
-        setNext(res.total)
+        setNext(res.next)
         setOperation(res.operation)
-        setTotal(0)
+        setTotal(res.total)
+      }
+      if (e.target.outerText === '.') {
+        const obj = {total, next, operation}
+        console.log(obj)
+        const res = calculate(obj, e.target.outerText);
+        console.log(res)
+        if (Object.entries(res).length !== 0) {
+          setNext(res.next===undefined ? next : res.next)
+          setOperation(null)
+          setTotal(res.total === undefined ? total : res.total)
+        }
       }
 
   };
@@ -53,7 +73,8 @@ export function Calculator() {
   };
 
   useEffect(() => {
-    prevCountRef.current = next;
+      const obj={total, next, operation}
+    console.log(obj)
   });
 
   const isNumber=(item) => !!item.match(/[0-9]+/)
@@ -61,7 +82,7 @@ export function Calculator() {
     <div className="calculator">
       <div className="calculator-body">
         <div className="input-val">
-          <input type="text" value={next} onChange={onChange} />
+          <input type="text" value={next===null? total : next} onChange={onChange} />
         </div>
       </div>
       <div className="calculator-body">
