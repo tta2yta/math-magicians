6b4bc66d8cc84f6948abfca290fe573fb117bd97
tta2yta <tta2yta@gmail.com>
2021-08-17 13:35:47 +0300

    Add content to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,45 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
-    <h3>Home page</h3>
+  <div className="home">
+    <h1>Math Magicians</h1>
+    <h3>Welcome to our page!</h3>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui o!cia deserunt mollit anim id est laborum. Ipsu Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident,
+        sunt in culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui o!cia deserunt mollit anim id est laborum. Ipsu Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident,
+        sunt in culpa qui o!cia deserunt mollit anim id est laborum. Ipsu
+      </p>
+    </div>
   </div>
 );
 
