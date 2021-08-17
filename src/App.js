c2a740e52a0839eac142c4672512f33a3964a447
tta2yta <tta2yta@gmail.com>
2021-08-17 12:39:08 +0300

    Add route about and qoutes in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Router, Switch } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/calculatorHooks';
 import NavBar from './components/NavBar';
+import About from './pages/about';
+import Qoutes from './pages/Qoutes';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -17,6 +19,12 @@ export default class App extends React.Component {
         <Switch>
           <Router path="/">
             <Calculator />
+          </Router>
+          <Router path="/about">
+            <About />
+          </Router>
+          <Router path="/Qoutes">
+            <Qoutes />
           </Router>
         </Switch>
       </div>
