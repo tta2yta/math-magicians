bf8e9df7c6cc516e3cf1e51afbd48985b73ac54e
tta2yta <tta2yta@gmail.com>
2021-08-18 11:28:09 +0300

    Add test cases in operate.js file

---
+++ src/test/operate.test.js
@@ -0,0 +1,20 @@
+import operate from "../logic/operate"
+
+test('adds two numbers', () => {
+    expect(operate(4, 5,  "+")).toEqual("9");
+  });
+
+  test('subtratc two numbers', () => {
+    expect(operate(4, 5,  "-")).toEqual("-1");
+  });
+
+  test('multiply two numbers', () => {
+    expect(operate(4, 5,  "x")).toEqual("20");
+  });
+  test('divide two numbers', () => {
+    expect(operate(5, 5,  "÷")).toEqual("1");
+  });
+
+  test('percentage of a number', () => {
+    expect(operate(4, 5,  "%")).toEqual("4");
+  });
