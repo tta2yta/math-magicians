ea2fdf7041c06729bf5cdddc2faf000a86bb3e08
tta2yta <tta2yta@gmail.com>
2021-08-19 23:55:37 +0300

    Add navbar test file

---
+++ src/test/components/navbar.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from '../../components/NavBar';
